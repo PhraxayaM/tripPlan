28fc3da4cfc609ac5e6a517f29a3a5490d6f4a1d
MattHew Phraxayavong <mphraxa@gmail.com>
2019-07-09 00:45:40 -0700

    models updated

--- tripPlan/Models/mapItem.swift
+++ tripPlan/Models/mapItem.swift
@@ -11,27 +11,32 @@ import UIKit
 public class mapItem: NSObject, NSCoding {
     
     var name: String
-  
-
+    var xLoc: String?
+    var yLoc: String?
+    
     enum Keys: String {
         case name = "name"
-    
+        case xLoc = "xLoc"
+        case yLoc = "yLoc"
+        
     }
     
-    init(name: String) {
-
-        
+    init(name: String, xLoc: String?, yLoc: String?) {
+      
         self.name = name
-
+        self.xLoc = xLoc
+        self.yLoc = yLoc
     }
     
     public func encode(with aCoder: NSCoder) {
         aCoder.encode(name, forKey: "name")
-      
+        aCoder.encode(xLoc, forKey: "xLoc")
+        aCoder.encode(yLoc, forKey: "yLoc")
     }
-
     public required init?(coder aDecoder: NSCoder) {
         name = aDecoder.decodeObject(forKey: "name") as! String
+        xLoc = aDecoder.decodeObject(forKey: "xLoc") as? String
+        yLoc = aDecoder.decodeObject(forKey: "yLoc") as? String
         super.init()
     }
     
