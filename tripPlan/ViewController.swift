62973ae2da9c852d4c3074effae2510e8a1972df
MattHew Phraxayavong <mphraxa@gmail.com>
2019-07-08 21:33:23 -0700

    commit

---
+++ tripPlan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tripPlan
+//
+//  Created by MattHew Phraxayavong on 7/8/19.
+//  Copyright © 2019 MattHew Phraxayavong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
